153eebfd13f03345d88ffc2f75360279c7a740f1
Łukasz <luqa91@hotmail.com>
2017-02-10 20:15:53 +0100

    Wprowadzenie button search, search css, poczatek session, isessionmanagera

---
+++ Sklep new/Infrasctructure/ISessionManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sklep_new.Infrasctructure
+{
+    interface ISessionManager
+    {
+        T Get<T>(string key);
+        void Set<T>(string name, T value);
+        void Abandon();
+        T TryGet<T>(string key);
+
+    }
+}
---
+++ Sklep new/Infrasctructure/SessionManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+/*
+namespace Sklep_new.Infrasctructure
+{
+    public class SessionManager : ISessionManager
+    {
+        private HttpSessionStateBase
+        public void Abandon()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set<T>(string name, T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T TryGet<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}*/
